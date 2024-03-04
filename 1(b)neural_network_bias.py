ffa3db441e92d2b3fed442ab5bf52b30edd4b873
CODEWITHSIDDHESH <131847600+Siddheshhh789@users.noreply.github.com>
2024-03-04 23:17:26 +0530

    Update 1(b)neural_network_bias.py

--- 1(b)neural_network_bias.py
+++ 1(b)neural_network_bias.py
@@ -1,27 +1,27 @@
 # number of elements as input 
-n = float(input("Enter number of elements: "))
+n = int(input("Enter number of elements: ")) 
 
 print("Enter the inputs")
-inputs = []
-# creating an empty list for inputs3
+inputs = []  # creating an empty list for inputs
+# iterating till the range 
+for i in range(0, n): 
+    ele = float(input()) 
+    inputs.append(ele)  # adding the element 
 
-# iterating till the range
-for i in range(0, n):
-    ele = float(input())
-    inputs.append(ele)  # adding the element
-print(inputs)
+print(inputs) 
 
 print("Enter the weights")
 # creating an empty list for weights
-weights = []
-# iterating till the range
-for i in range(0, n):
-    ele = float(input())
-    weights.append(ele)  # adding the element
-print(weights)
+weights = [] 
+# iterating till the range 
+for i in range(0, n): 
+    ele = float(input()) 
+    weights.append(ele)  # adding the element 
+
+print(weights) 
 
 print("The net input can be calculated as Yin = x1w1 + x2w2 + x3w3")
-# creating an empty list for Yin
+
 Yin = []
 for i in range(0, n):
     Yin.append(inputs[i] * weights[i])
